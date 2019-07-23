76eb26fc450214298276e301966e1cd48fe206a5
Leo Cornelius <kegcoingold@gmail.com>
2019-07-23 12:00:50 +0100

    Create balance handler
    Create a btc wallet address,
    create the kegcoin wallet address,
    # or
    get BTC wallet balance
    get kegcoin wallet balance

---
+++ coinhandler.php
@@ -0,0 +1,27 @@
+<?php
+ use TurtleCoin\TurtleService;
+ require_once '/API/btcjsonRPCClient.php';
+ error_reporting(E_WARNING | E_PARSE);
+ $config = [
+    'rpcHost'     => 'http://localhost',
+    'rpcPort'     => 5050,
+    'rpcPassword' => 'passw0rd',
+  ];
+ $bitcoin = new jsonRPCClient('http://username:password@127.0.0.1:8332/');
+ $kegcoin = new TurtleService($config);
+ $btcbalance=$bitcoin->getbalance($_SESSION['username'],4);
+ $address = ($_SESSION['username'],4);
+ $kegaddress=$kegcoin->getBalance($address);
+	if (isset($address)){
+		$btcaddress=$bitcoin->getaccountaddress($_SESSION['username']);
+    $address = ($_SESSION['username'],4);
+    $kegaddress=$kegcoin->getBalance($address);
+	}
+	else
+	{
+   $spendSecretKey = null;
+   $spendPublicKey = null;
+	 $kegaddress=$kegcoin->createAddress($spendSecretKey, $spendPublicKey);
+	 $btcaddress=$bitcoin->getnewaddress($_SESSION['username']);
+	}
+?>
