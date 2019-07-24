6a44e713b7a1e7730979a2e3eefb6e750d5f207e
Leo Cornelius <kegcoingold@gmail.com>
2019-07-24 17:54:35 +0100

    Update coins - fix a few issues

--- coinhandler.php
+++ coinhandler.php
@@ -1,6 +1,7 @@
 <?php
  use TurtleCoin\TurtleService;
  require_once '/API/jsonRPCClient.php';
+include("db.php");
  error_reporting(E_WARNING | E_PARSE);
  $config = [
     'rpcHost'     => 'http://localhost',
@@ -10,18 +11,19 @@
  $bitcoin = new jsonRPCClient('http://username:password@127.0.0.1:8332/');
  $kegcoin = new TurtleService($config);
  $btcbalance=$bitcoin->getbalance($_SESSION['username'],4);
- $address = ($_SESSION['username'],4);
- $kegaddress=$kegcoin->getBalance($address);
-	if (isset($address)){
-		$btcaddress=$bitcoin->getaccountaddress($_SESSION['username']);
+ $btcaddress = ($_SESSION['username'],4);
+ $kegcoinaddress = getKegAddress($_SESSION['username']);
+ $kegbalance=$kegcoin->getBalance($kegaddress);
+ if (isset($btcaddress)){
+    $btcaddress=$bitcoin->getaccountaddress($_SESSION['username']);
     $address = ($_SESSION['username'],4);
     $kegaddress=$kegcoin->getBalance($address);
-	}
-	else
-	{
+ }else
+ {
    $spendSecretKey = null;
    $spendPublicKey = null;
-	 $kegaddress=$kegcoin->createAddress($spendSecretKey, $spendPublicKey);
-	 $btcaddress=$bitcoin->getnewaddress($_SESSION['username']);
-	}
+   $kegaddress=$kegcoin->createAddress($spendSecretKey, $spendPublicKey);
+   setKegcoinAddress($_SESSION['username'],$kegaddress);
+   $btcaddress=$bitcoin->getnewaddress($_SESSION['username']);
+ }
 ?>
