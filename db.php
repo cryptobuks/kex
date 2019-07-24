ffeb2b7333fdc5f693a84e636abefb609f987cef
Leo Cornelius <kegcoingold@gmail.com>
2019-07-24 18:38:12 +0100

    Update db.php
    * Add set BTC balance
    * Add set KEG balance
    * Add set BTC address
    * Rename getBtcAddress() to getBitcoinAddress()

--- db.php
+++ db.php
@@ -42,12 +42,21 @@ function getKegcoinBalance($username)
 	$balance = $sql;
 	return $balance;		    
 }
-function getKegAddress($username) {	
-	$username = mysql_real_escape_string($username);
-	//query
-	 $sql = mysql_query("SELECT kegcoin FROM addresses where username = '".$username.);
-	$address = $sql;
-	return $address;
+function setBitcoinBalance($username, $balance) {
+	$sql = "UPDATE balances SET bitcoin=".$balance." WHERE username=".$username.;
+	if ($conn->query($sql) === TRUE) {
+    		echo "Record updated successfully";
+	} else {
+    	       echo "Error updating record: " . $conn->error;
+	}
+}
+function setKegcoinBalance($username, $balance) {
+	$sql = "UPDATE balances SET kegcoin=".$balance." WHERE username=".$username.;
+	if ($conn->query($sql) === TRUE) {
+    		echo "Record updated successfully";
+	} else {
+    	       echo "Error updating record: " . $conn->error;
+	}
 }
 function setKegAddress($username, $address) {
 	$username = mysql_real_escape_string($username);
@@ -68,5 +77,26 @@ function setKegAddress($username, $address) {
     		echo "Error updating record: " . $conn->error;
 		}
 	}
-		
+}		
+function setKegAddress($username, $address) {
+	$username = mysql_real_escape_string($username);
+	//query
+	$sql = mysql_query("SELECT bitcoin FROM addresses where username = '".$username. LIMIT 1");
+	$rows = mysql_num_rows($sql);
+	$kegcoinAddress = getKegcoinAddress($username);
+	if ($rows = 0) 
+	{
+		$sql = "INSERT INTO addresses (username, kegcoin, bitcoin)
+       	        VALUES ($username, $kegcoinAddress, $address)";
+		}
+		else
+		{
+		$sql = "UPDATE addresses SET bitcoin=".$address." WHERE username=".$username.;
+		if ($conn->query($sql) === TRUE) {
+    			echo "Record updated successfully";
+		} else {
+    		echo "Error updating record: " . $conn->error;
+		}
+	}
+}		
 ?>
