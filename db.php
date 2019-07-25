d98f4e0c6ac156b2cb9dd58125fde51a54aed291
Leo Cornelius <kegcoingold@gmail.com>
2019-07-26 00:03:56 +0100

    Update db.php
    * Minor fixes
    * removed get bitcoin address because we generate a new one each time a user deposits (for simplicity)

--- db.php
+++ db.php
@@ -21,7 +21,7 @@ function user_login($username, $password)
 			else
 			{
 				$_SESSION['username']=$username;
-				header("Location: trade.php");
+				header("Location: home.php");
 			}
 }
 function getBtcBalance($username)
@@ -44,7 +44,7 @@ function getKegcoinBalance($username)
 }
 function setBitcoinBalance($username, $balance) {
 	$sql = "UPDATE balances SET bitcoin=".$balance." WHERE username=".$username.;
-	if ($conn->query($sql) === TRUE) {
+	if (mysql_query($sql) === TRUE) {
     		echo "Record updated successfully";
 	} else {
     	       echo "Error updating record: " . $conn->error;
@@ -52,7 +52,7 @@ function setBitcoinBalance($username, $balance) {
 }
 function setKegcoinBalance($username, $balance) {
 	$sql = "UPDATE balances SET kegcoin=".$balance." WHERE username=".$username.;
-	if ($conn->query($sql) === TRUE) {
+	if (mysql_query($sql) === TRUE) {
     		echo "Record updated successfully";
 	} else {
     	       echo "Error updating record: " . $conn->error;
@@ -67,36 +67,15 @@ function setKegAddress($username, $address) {
 	{
 		$sql = "INSERT INTO addresses (username, kegcoin)
        	        VALUES ($username, $address)";
-		}
-		else
-		{
+	}else {
 		$sql = "UPDATE addresses SET kegcoin=".$address." WHERE username=".$username.;
-		if ($conn->query($sql) === TRUE) {
+		if (mysql_query($sql) === TRUE) {
     			echo "Record updated successfully";
 		} else {
     		echo "Error updating record: " . $conn->error;
-		}
 	}
 }		
-function setKegAddress($username, $address) {
-	$username = mysql_real_escape_string($username);
-	//query
-	$sql = mysql_query("SELECT bitcoin FROM addresses where username = '".$username. LIMIT 1");
-	$rows = mysql_num_rows($sql);
-	$kegcoinAddress = getKegcoinAddress($username);
-	if ($rows = 0) 
-	{
-		$sql = "INSERT INTO addresses (username, kegcoin, bitcoin)
-       	        VALUES ($username, $kegcoinAddress, $address)";
-		}
-		else
-		{
-		$sql = "UPDATE addresses SET bitcoin=".$address." WHERE username=".$username.;
-		if ($conn->query($sql) === TRUE) {
-    			echo "Record updated successfully";
-		} else {
-    		echo "Error updating record: " . $conn->error;
-		}
-	}
+function getTrades($market) {
+        $market = mysql_real_escape_string($market);
 }		
 ?>
